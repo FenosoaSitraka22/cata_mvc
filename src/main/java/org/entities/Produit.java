1981f63547041f7445df9e1b0a55b3f78c4e466e
FenosoaSitraka22 <fenosoratsimba@gmail.com>
2020-08-02 15:53:41 +0200

    new project

---
+++ src/main/java/org/entities/Produit.java
@@ -0,0 +1,56 @@
+package org.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Entity;
+
+import com.sun.istack.NotNull;
+
+@Entity 
+
+public class Produit {
+	@Id @GeneratedValue 
+ private Long id;
+	
+ private String designation;
+ private Double Prix;
+ @NotNull
+ private int qantite;
+ 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public Double getPrix() {
+		return Prix;
+	}
+	public void setPrix(Double prix) {
+		Prix = prix;
+	}
+	public int getQantite() {
+		return qantite;
+	}
+	public void setQantite(int qantite) {
+		this.qantite = qantite;
+	}
+	public Produit(String designation, Double prix, int qantite) {
+		super();
+		this.designation = designation;
+		Prix = prix;
+		this.qantite = qantite;
+	}
+	public Produit() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
